48b9fc301c3fdf9e65670059a17f5438b2dca9cf
Corey Ferland <ferlandc@gmail.com>
2020-01-31 14:00:37 -0500

    accounts backend

--- account.rb
+++ account.rb
@@ -37,7 +37,33 @@ class Account
     else
       puts "Error: your balance is less than 0"
     end
-  end
+    end
+
+    def save_account
+      file = File.open(File.dirname(__FILE__ ) + '/accounts.txt', "a")
+      file.puts("#{@account_num} #{@balance} #{@account_type} #{@account_pin} #{@customer_num} #{@time_stamp}")
+      file.close
+    end
+
+    def update_account
+      acct_numbers = []
+      balances = []
+      acct_types = []
+      acct_pins = []
+      cust_nums = []
+      time_stamps = []
+      file = File.open(File.dirname(__FILE__) + '/accounts.txt', "r")
+      file.each_line do |line|
+        acct_numbers << line.split(" ")[0]
+        balances << line.split(" ")[1]
+        acct_types << line.split(" ")[2]
+        acct_pins << line.split(" ")[3]
+        cust_nums << line.split(" ")[4]
+        time_stamps << line.split(" ")[5]
+      end
+
+    end
+
   end
   class Checking < Account
   
