7cc02f1dd47ca8d2d4c86c423dd49a21f817e87b
Connor Lagana <connor.lagana@gmail.com>
2020-01-31 10:58:54 -0500

    coreyyyyy

--- Customer.rb
+++ Customer.rb
@@ -1,10 +1,14 @@
-require account.rb
+load "account.rb"
+
+# counter = 1
+
 class Customer
   attr_accessor :customer_number, :first_name, :last_name, :ssn, :address, :account_numbers
+  
 
-
-  def initialize(customer_number, first_name, last_name, ssn, address, account_numbers)
-    @customer_number = customer_number
+  def initialize(first_name, last_name, ssn, address, account_numbers)
+    random_number2 = rand(999999999)
+    @customer_number = random_number2
     @first_name = first_name
     @last_name = last_name
     @ssn = ssn
@@ -14,21 +18,29 @@ class Customer
   
   def new_account()
     puts("What type of account would you like to open? (Checking = 'C', Savings = 'S')")
-    type = gets.chomp
+    type = gets.chomp.downcase
 
     puts("How much would you like to depoist?")
     balance = gets.chomp.to_i - 3
 
+    puts("What is your pin number?")
+    pin = gets.chomp.to_i
+
     time1 = Time.now
+
+    random_number = rand(999999999)
+    
 
     #SOME RANDOM GENERATOR FUNCTION FOR ACCOUNT NUMBERS
 
-    if type.downcase = "c"
+    if type = "c"
       #open checking account
       
-      new_acct = Account.new(@customer_number, balance, account_num, type, time1.inspect)
+      new_acct = Account.new(@customer_number, balance, random_number, type, time1.inspect, pin)
+
+      account_numbers.append(random_number)
       
-    elsif typ.downcase = 's'
+    elsif type = 's'
       #open savings account
     else
       puts("Please select either Checking = 'C' or Savings = 'S'")
@@ -37,3 +49,6 @@ class Customer
 
 end
 
+connor = Customer.new("Connor", "Lagana", "420-69-1000", "1 Fuckme Ave", [5, 19])
+connor.new_account()
+p (connor)
